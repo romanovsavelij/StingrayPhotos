8ea40e875b89f74b7cf45cbdfe4a87cadc48e8fa
romanovsavelij <romanovsavelij@mail.ru>
2020-07-19 21:58:18 +0300

    Get image test

--- get_image/tests.py
+++ get_image/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from http import HTTPStatus as Status
+from utils.get_unique_key import get_unique_key
+from io import BytesIO
 
-# Create your tests here.
+
+class GetImageTestCase(TestCase):
+    def test_no_images(self):
+        key = get_unique_key()
+
+        img1 = BytesIO(b'first_image')
+        img1.name = 'my_image.jpg'
+
+        img2 = BytesIO(b'second_image')
+        img2.name = 'my_image.jpg'
+
+        img3 = BytesIO(b'third_image')
+        img3.name = 'my_image.jpg'
+
+        response = self.client.post(f'/upload/?key={key}', {'images': (img1, img2, img3)})
+        self.assertEqual(response.status_code, Status.OK)
+
+        response = self.client.get(f'/get_image/?key={key}')
+        self.assertEqual(response.status_code, Status.OK)
+        self.assertEqual(response.content, img1.getvalue())
+
+        response = self.client.get(f'/get_image/?key={key}')
+        self.assertEqual(response.status_code, Status.OK)
+        self.assertEqual(response.content, img2.getvalue())
+
+        response = self.client.get(f'/get_image/?key={key}')
+        self.assertEqual(response.status_code, Status.OK)
+        self.assertEqual(response.content, img3.getvalue())
+
+        response = self.client.get(f'/get_image/?key={key}')
+        self.assertEqual(response.status_code, Status.OK)
+        self.assertEqual(response.content, img1.getvalue())
